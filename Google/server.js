3383c067fbef5e6d7cdcf62258d220a61f23d451
amatranga <amatranga892@gmail.com>
2017-08-05 16:34:40 -0700

    [Fix] Fix deprecation warnings in Google

--- Google/server.js
+++ Google/server.js
@@ -3,8 +3,8 @@ const app = express()
 
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const session = require('express-session');
+const bodyParser = require('body-parser').urlencoded({ extended: true });
+const session = require('express-session')({ secret: 'Whatever you want your session secret to be', resave: true, saveUninitialized: true});
 const passport = require('passport');
 const flash = require('connect-flash');
 const mongoose = require('mongoose');
@@ -18,12 +18,12 @@ mongoose.connect(dbConfig.url);
 //set up express server
 app.use(morgan('dev'));  //all requests will now be logged to the server
 app.use(cookieParser());  //reads cookies, which are needed for auth
-app.use(bodyParser.json());  //get information for html forms
+app.use(bodyParser);  //get information for html forms
 
 app.set('view engine', 'ejs')  //sets up ejs for templating
 
 //everything here is required for passport
-app.use(session({ secret: 'Whatever you want your session secret to be'} ));
+app.use(session);
 //your session secret will be used o sign the session cookie to prevent tampering
 app.use(passport.initialize());
 app.use(passport.session());  //persistent login sessions
