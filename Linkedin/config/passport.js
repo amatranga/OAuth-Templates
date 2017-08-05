8e9413c0593d99e4e9f37936e2b265f4cd636a67
amatranga <amatranga892@gmail.com>
2017-08-05 14:01:11 -0700

    [Feat] Implement config for linkedin

---
+++ Linkedin/config/passport.js
@@ -0,0 +1,51 @@
+const passport = require('passport');
+const LinkedinStrategy = require('passport-linkedin').Strategy;
+
+const User = require('../app/models/user');
+const auth = require('./auth');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+passport.use('linkedin', new LinkedinStrategy({
+  clientId: auth.linkedinAuth.LINKEDIN_CLIENT_ID,
+  clientSecret: auth.linkedinAuth.LINKEDIN_CLIENT_SECRET,
+  callbackURL: auth.linkedinAuth.LINKEDIN_CALLBACKURL
+}, 
+  (token, refreshToken, profile, done) => {
+    User.findOne({
+      'linkedin.id': profile.id,
+      'linkedin.email': profile.email,
+      'linkedin.name': profile.email,
+      'linkedin.token': profile.token
+    }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (user) {
+        return done(null, user);
+      } else {
+        const newUser = new User();
+        newUser.linkedin.id = profile.id;
+        newUser.linkedin.email = profile.emails[0].value;
+        newUser.linkedin.name = profile.name;
+        newUser.linkedin.token = profile.token;
+
+        newUser.save((err) => {
+          if (err) {
+            throw err;
+          }
+          return done(null, newUser);
+        });
+      }
+    });
+  }));
+
+module.exports = passport;
